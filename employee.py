81382c22c4d534eb0e9804ef20d92a694bf324a8
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-10-18 17:18:18 +0600

    lalalaa

---
+++ employee.py
@@ -0,0 +1,31 @@
+print("\033c")
+
+class Person:
+    
+    
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+        
+        
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+        
+        
+class Employee(Person):
+    
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+        
+        
+        Person.__init__(self, name, idnumber)
+        
+        
+a = Employee("Rifa", 4, 100000000, "Manager")
+
+a.display()
+print(a.salary)
+
+print(a.post)
